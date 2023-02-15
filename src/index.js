e000f3602dd8c04cf529b6876cc8b169d1137dd4
juli_ko <yuliakolomiets09@gmail.com>
2023-02-16 00:24:44 +0300

    feat: number in words

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return "zero";
+    let obj_1 = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    };
+    let obj_10 = {
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+    };
+    let obj_20 = {
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+    };
+
+    let result = [];
+
+    if (Math.floor(number / 100) !== 0) {
+        result.push(obj_1[Math.floor(number / 100)]);
+        result.push("hundred");
+    }
+
+    if (Math.floor((number % 100) / 10) === 1) {
+        result.push(obj_10[number % 100]);
+    }
+
+    if (Math.floor((number % 100) / 10) > 1) {
+        result.push(obj_20[Math.floor((number % 100) - (number % 10))]);
+    }
+
+    if (Math.floor((number % 100) / 10) !== 1 && number % 10 !==0) {
+        result.push(obj_1[number % 10]);
+    }
+
+    return result.join(' ');
 }
